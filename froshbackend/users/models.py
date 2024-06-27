774632c65f83766392e17528d86b5128d4da3220
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-06-28 02:32:48 +0530

    configuring more stuff

--- froshbackend/users/models.py
+++ froshbackend/users/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from django.test import TestCase
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.postgres.fields import ArrayField
+
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from .manager import CustomUserManager
+def default_events():
+    return []
+
 class User(AbstractUser):
     username = None
     image = models.URLField()
     registration_id = models.CharField(unique=True, max_length=20)
     secure_id = models.CharField(unique=True, max_length=8, null=True, blank=True)
-    events = ArrayField(base_field=models.CharField(max_length=60), max_length=50, blank=True, default=list)
     qr = models.URLField(blank=True)
+    # events = models.JSONField(default=default_events, blank=True) 
     USERNAME_FIELD = "registration_id"
     REQUIRED_FIELD = ['image', 'qr']
     objects = CustomUserManager()
