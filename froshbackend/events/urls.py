82f2624598cea380c0924c08c95bd809c2a647af
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-07-12 14:12:47 +0530

    correct event urls

--- froshbackend/events/urls.py
+++ froshbackend/events/urls.py
@@ -5,11 +5,13 @@ from .views import EventList
 from .views import EventView
 from .views import qr_scanner_view,process_qr
 urlpatterns = [
-    path('list/', views.EventList),
+    # path('list/', views.EventList),
+    path('list/', EventList.as_view(), name='event-list'),
     path('<int:pk>/', views.EventView),
     # path("scan/",views.scanner, name='scanner'),
     path('scan/', views.qr_scanner_view, name='qr_scanner'),
     path('scan/process/', views.process_qr, name='process_qr'),
+    path('book-ticket/<int:user_id>/', views.book_ticket, name='book_ticket'),
 
     # path('get_qr_data/', views.get_qr_data, name='get_qr_data'),
 ]
