4c1a641494b2d03150b3efc884c55cb2dbaaf20a
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-07-12 14:11:50 +0530

    user serializer

--- froshbackend/events/serializers.py
+++ froshbackend/events/serializers.py
@@ -1,4 +1,4 @@
-from .models import Events
+from .models import User,Events
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 class eventSerializer(serializers.ModelSerializer):
@@ -6,3 +6,7 @@ class eventSerializer(serializers.ModelSerializer):
         model=Events
         fields="__all__"
         
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'is_booked', 'events']  
