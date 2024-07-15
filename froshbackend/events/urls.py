37e7eca2d203e1273ebd0d355bb469e7133675b9
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-07-16 00:18:31 +0530

    Update urls.py

--- froshbackend/events/urls.py
+++ froshbackend/events/urls.py
@@ -3,14 +3,15 @@ from django.urls import path
 from . import views
 from .views import EventList
 from .views import EventView
-from .views import qr_scanner_view,process_qr
+from .views import qr_scanner_view
 urlpatterns = [
     # path('list/', views.EventList),
     path('list/', EventList.as_view(), name='event-list'),
     path('<int:pk>/', views.EventView),
     # path("scan/",views.scanner, name='scanner'),
+    # path('scan/', views.qr_scanner_view, name='qr_scanner'),
+    # path('scan/process/', views.process_qr, name='process_qr'),
     path('scan/', views.qr_scanner_view, name='qr_scanner'),
-    path('scan/process/', views.process_qr, name='process_qr'),
     path('book-ticket/<int:user_id>/', views.book_ticket, name='book_ticket'),
 
     # path('get_qr_data/', views.get_qr_data, name='get_qr_data'),
