224f3a98770d8a0fbb0d85837d4a3fae1d803286
th3m4nd4r1n <mdua_be23@thapar.edu>
2024-07-26 05:49:43 +0530

    Added ThreeJs and a temporary model on a canvas just for the sake of it.  Added const rotation along the y axis for the mean while

--- frosh/src/components/Home.jsx
+++ frosh/src/components/Home.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
+import {Canvas, useFrame} from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+import {OrbitControls} from "@react-three/drei";
 
-const Home =()=>{
+function Model() {
+  const { scene } = useGLTF('../../public/modeljust.gltf');
+  const ref = useRef();
+  useFrame(() => {
+    ref.current.rotation.y += 0.01;
+  })
+  return (
+    <mesh position={[0,0,0]}>
+  <primitive object={scene} scale={0.005} ref={ref} />
+    </mesh>
+  )
+}
+const Home = () => {
+
     return(
         <div className="home" id="Home">
             <div className="frosh-main-img">
 
             </div>
+            <span className="compass">
+                <Canvas>
+                        <ambientLight intensity={100} />
+                        <Model />
+                      
+                </Canvas>
+
+            </span>
             <div className="frosh-tag">
                 <h1>Navigating Through Timeless Trails</h1>
             </div>
