467d2cb70b6c6e7a621cc9fe5ff97a1da2431e52
th3m4nd4r1n <mdua_be23@thapar.edu>
2024-07-28 11:33:13 +0530

    new photo added saying "frsh" compass added and motion added

--- frosh/src/components/Home.jsx
+++ frosh/src/components/Home.jsx
@@ -1,17 +1,34 @@
 import React, { useRef } from "react";
 import {Canvas, useFrame} from "@react-three/fiber";
-import { useGLTF } from "@react-three/drei";
+import { Environment, useGLTF } from "@react-three/drei";
 import {OrbitControls} from "@react-three/drei";
+import { AmbientLight, MeshBasicMaterial } from "three";
 
 function Model() {
-  const { scene } = useGLTF('../../public/modeljust.gltf');
+  const { scene } = useGLTF('../../public/compass web.gltf');
   const ref = useRef();
   useFrame(() => {
-    ref.current.rotation.y += 0.01;
+      ref.current.position.y=0.52;
+      const y = window.scrollY;
+      console.log(y);
+    if (y>=0 && y<=1){
+      ref.current.rotation.y+=0.01;
+    }
+    else if(y>=2009 && y<=2010){
+      ref.current.rotation.y-=0.01;
+    }
+    else{
+      ref.current.rotation.y = -y * 0.0062;
+      ref.current.position.x = Math.sin(y*0.003112) * 1.2 ;
+      
+    }
+
+    
+
   })
   return (
     <mesh position={[0,0,0]}>
-  <primitive object={scene} scale={0.005} ref={ref} />
+  <primitive object={scene} scale={0.3} ref={ref} />
     </mesh>
   )
 }
@@ -19,17 +36,20 @@ const Home = () => {
 
     return(
         <div className="home" id="Home">
-            <div className="frosh-main-img">
+            <div className="main_landing">
 
             </div>
             <span className="compass">
                 <Canvas>
-                        <ambientLight intensity={100} />
+                 <Environment preset="studio" environmentIntensity={0.5} />
                         <Model />
+                        <OrbitControls enablePan={false} enableZoom={false} enableDamping={true} /> 
                       
                 </Canvas>
 
             </span>
+
+         
             <div className="frosh-tag">
                 <h1>Navigating Through Timeless Trails</h1>
             </div>
