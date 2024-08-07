320d1da360fd2e661c8c51e122102e3add7bc90f
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-08-08 01:55:01 +0530

    Update settings.py

--- froshbackend/froshbackend/settings.py
+++ froshbackend/froshbackend/settings.py
@@ -96,19 +96,21 @@ TEMPLATES = [
 
 
 WSGI_APPLICATION = 'froshbackend.wsgi.application'
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 
 # Database
 # https://docs.djangopr oject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'frosh1',
-        'USER': 'myuser1',
-        'PASSWORD': 'passwd',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',   
-        
+        'NAME': 'defaultdb',
+        'USER': 'doadmin',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '25060',
     }
 }
 AUTH_USER_MODEL = 'users.User' 
