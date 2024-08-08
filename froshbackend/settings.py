c39141f61d1648db4b3c040e884acd935bce0a74
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-08-08 05:30:21 +0530

    Update settings.py

--- froshbackend/settings.py
+++ froshbackend/settings.py
@@ -36,6 +36,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS=True
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 INSTALLED_APPS = [
     "users",
@@ -139,7 +141,7 @@ USE_I18N = True
 USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
+ 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
