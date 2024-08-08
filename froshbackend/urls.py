1a036362f51c2fb59be995e39fa86e293083d92b
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-08-08 05:56:42 +0530

    Update urls.py

--- froshbackend/urls.py
+++ froshbackend/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from froshbackend import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +28,6 @@ urlpatterns = [
     path('', include('hoods.urls')),
     
 ]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
