a62ec611e63b32def0165e94bdb0f86ee2629e92
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-08-08 06:05:04 +0530

    Update urls.py

--- froshbackend/urls.py
+++ froshbackend/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls import include, url
+from django.conf.urls import include
 # from django.conf.urls.static import static
 # from froshbackend import settings
 
