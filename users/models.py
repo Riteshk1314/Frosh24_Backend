04a7f1ce886075e4511e6b472a8eda19d87d343e
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-08-12 03:05:04 +0530

    trying to fix scanning before F'25

--- users/models.py
+++ users/models.py
@@ -101,11 +101,11 @@ class User(AbstractUser):
 
         super().save(*args, **kwargs)
 
-#mailing is commented for the purpose of setting up db 
-        # if is_new and raw_password:
-        #     send_credentials_email(self.email, self.registration_id, raw_password)
+# mailing is commented for the purpose of setting up db 
+        if is_new and raw_password:
+            send_credentials_email(self.email, self.registration_id, raw_password)
             
-            
+             
             
             
             
