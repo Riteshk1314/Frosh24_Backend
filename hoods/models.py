7096e72ad9bbf12420c07f5def846b6102a3990a
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-11-04 14:34:34 +0530

    models and apps hoods

--- hoods/models.py
+++ hoods/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-
 class Hoods(models.Model):
     hood_id = models.AutoField(primary_key=True, blank=True)
     hood_name = models.CharField(max_length=100)
@@ -8,7 +6,6 @@ class Hoods(models.Model):
     image = models.URLField(blank=True)
     member_count = models.IntegerField(default=0)
     points = models.IntegerField(default=0)
-    # is_booking = models.BooleanField(default=True)
 
     def __str__(self):
         return self.hood_name
